1f4309157982bc9404da368fd662decefd150a3e
Maxheart <samuelhenrykurniawan@yahoo.com>
2020-03-17 00:20:55 +0800

    Refactor and add user access control function

--- src/models/user_models.rs
+++ src/models/user_models.rs
@@ -17,7 +17,7 @@ struct UserRowWrapper {
 }
 
 #[derive(Insertable)]
-#[table_name="users"]
+#[table_name = "users"]
 struct NewUserRowWrapper<'a> {
     pub username: &'a str,
     pub display_name: &'a str,
@@ -33,11 +33,17 @@ pub struct User {
 }
 
 impl User {
-    pub fn sign_up(username: String, display_name: String, password: String, role: String) -> Result<User, io::Error> {
+    pub fn create_new_admin(username: String, display_name: String, password: String) -> Result<User, io::Error> {
         if User::is_username_exists(&username) {
             panic!("Username exists")
         }
-        let user = User::create_object_from(username, display_name, password, role);
+        let hashed_password = bcrypt::hash(password).unwrap();
+        let user = User {
+            username,
+            display_name,
+            hashed_password,
+            role: UserRole::from_string(String::from("admin")),
+        };
         user.insert_to_database();
         Ok(user)
     }
@@ -52,7 +58,10 @@ impl User {
 
     fn get_user(username: &String) -> Result<User, io::Error> {
         let connection = establish_connection();
-        let user_row_wrapper = users::table.filter(users::username.eq(&username)).first::<UserRowWrapper>(&connection).expect("User not found");
+        let user_row_wrapper = users::table
+            .filter(users::username.eq(&username))
+            .first::<UserRowWrapper>(&connection)
+            .expect("User not found");
         Ok(User {
             username: user_row_wrapper.username,
             display_name: user_row_wrapper.display_name,
@@ -61,20 +70,10 @@ impl User {
         })
     }
 
-    fn create_object_from(username: String, display_name: String, password: String, role: String) -> User {
-        let hashed_password = bcrypt::hash(password).unwrap();
-        User{
-            username,
-            display_name,
-            hashed_password,
-            role: UserRole::from_string(role)
-        }
-    }
-
     fn insert_to_database(&self) {
         let connection = establish_connection();
         diesel::insert_into(users::table)
-            .values(NewUserRowWrapper{
+            .values(NewUserRowWrapper {
                 username: self.username.as_ref(),
                 display_name: self.display_name.as_ref(),
                 hashed_password: self.hashed_password.as_ref(),
@@ -95,4 +94,8 @@ impl User {
 //    pub fn generate_jwt_token(&self) -> String {
 //
 //    }
+
+    pub fn is_allowed_to_access(&self, allowed_role: Vec<UserRole>) -> bool {
+        allowed_role.contains(&self.role)
+    }
 }
