8496f68c37a6ec18b58b151490d55f412360e83c
Maxheart <samuelhenrykurniawan@yahoo.com>
2020-08-22 17:14:19 +0800

    Add name parser tests

--- src/joueurs/joueurs_name_parser.rs
+++ src/joueurs/joueurs_name_parser.rs
@@ -64,3 +64,85 @@ impl NameParser {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    mod test_name_parser {
+        use crate::joueurs::joueurs_name_parser::NameParser;
+
+        fn test_parse_name(
+            name: &String, expected_first_name: &String, expected_last_name: &String
+        ) -> bool {
+            let mut parser = NameParser::create();
+            let player_name = parser.parse(name);
+
+            &player_name.first_name == expected_first_name
+                && &player_name.last_name == expected_last_name
+        }
+
+        #[test]
+        fn test_parse_comma_normal() {
+            let name = String::from("CAMPO, Sebastian");
+            let expected_first_name = String::from("Sebastian");
+            let expected_last_name = String::from("CAMPO");
+            assert_eq!(test_parse_name(&name, &expected_first_name, &expected_last_name), true);
+        }
+
+        #[test]
+        fn test_parse_comma_name_multiple_words() {
+            let name = String::from("DE JONG, Thiago De Oliveira");
+            let expected_first_name = String::from("Thiago De Oliveira");
+            let expected_last_name = String::from("DE JONG");
+            assert_eq!(test_parse_name(&name, &expected_first_name, &expected_last_name), true);
+        }
+
+        #[test]
+        fn test_parse_comma_name_with_dots() {
+            let name = String::from("DE. GRAAF, Jan C.");
+            let expected_first_name = String::from("Jan C.");
+            let expected_last_name = String::from("DE. GRAAF");
+            assert_eq!(test_parse_name(&name, &expected_first_name, &expected_last_name), true);
+        }
+
+        #[test]
+        fn test_parse_comma_name_with_hyphen() {
+            let name = String::from("DAHL-PAULSEN, Per-Helge");
+            let expected_first_name = String::from("Per-Helge");
+            let expected_last_name = String::from("DAHL-PAULSEN");
+            assert_eq!(test_parse_name(&name, &expected_first_name, &expected_last_name), true);
+        }
+
+        #[test]
+        fn test_parse_comma_no_first_name() {
+            let name = String::from("SOBEA,");
+            let expected_first_name = String::from("");
+            let expected_last_name = String::from("SOBEA");
+            assert_eq!(test_parse_name(&name, &expected_first_name, &expected_last_name), true);
+        }
+
+        #[test]
+        fn test_parse_bracket_normal() {
+            let name = String::from("ATHALIE (Calien)");
+            let expected_first_name = String::from("Calien");
+            let expected_last_name = String::from("ATHALIE");
+            assert_eq!(test_parse_name(&name, &expected_first_name, &expected_last_name), true);
+        }
+
+        #[test]
+        fn test_parse_bracket_no_first_name() {
+            let name = String::from("ATHALIE ()");
+            let expected_first_name = String::from("");
+            let expected_last_name = String::from("ATHALIE");
+            assert_eq!(test_parse_name(&name, &expected_first_name, &expected_last_name), true);
+        }
+
+        #[test]
+        fn test_parse_bracket_multiple_words_with_hyphen() {
+            let name = String::from("HANNIBAL TEST (Piotte-Geoffroy / De Haan)");
+            let expected_first_name = String::from("Piotte-Geoffroy / De Haan");
+            let expected_last_name = String::from("HANNIBAL TEST");
+            assert_eq!(test_parse_name(&name, &expected_first_name, &expected_last_name), true);
+        }
+    }
+}
+
