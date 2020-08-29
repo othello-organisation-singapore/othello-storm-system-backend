51edbc87dde216f04a29e1f6aeea0c54c7e4f57e
Maxheart <samuelhenrykurniawan@yahoo.com>
2020-08-29 18:11:15 +0800

    add player from and to serdemap

--- src/tournament_manager/player.rs
+++ src/tournament_manager/player.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use serde_json::{Map, Value};
+
 pub struct Player {
     pub joueurs_id: i32,
     pub first_name: String,
@@ -13,30 +15,30 @@ impl Player {
     pub fn from_hashmap(player_data: HashMap<String, String>) -> Result<Player, String> {
         let joueurs_id = player_data
             .get("joueurs_id")
-            .ok_or("Incomplete data")?
+            .ok_or("Incomplete player data")?
             .parse::<i32>()
-            .map_err(|_| String::from("Something is wrong"))?;
+            .map_err(|_| String::from("Id is not integer"))?;
 
         let first_name = player_data
             .get("first_name")
-            .ok_or("Incomplete data")?
+            .ok_or("Incomplete player data")?
             .to_string();
 
         let last_name = player_data
             .get("last_name")
-            .ok_or("Incomplete data")?
+            .ok_or("Incomplete player data")?
             .to_string();
 
         let country = player_data
             .get("country")
-            .ok_or("Incomplete data")?
+            .ok_or("Incomplete player data")?
             .to_string();
 
         let rating = player_data
             .get("rating")
-            .ok_or("Incomplete data")?
+            .ok_or("Incomplete player data")?
             .parse::<i32>()
-            .map_err(|_| String::from("Something is wrong"))?;
+            .map_err(|_| String::from("Rating is not integer"))?;
 
         Ok(Player{ joueurs_id, first_name, last_name, country, rating})
     }
@@ -48,6 +50,69 @@ impl Player {
         player_data.insert(String::from("last_name"), self.last_name.clone());
         player_data.insert(String::from("country"), self.country.clone());
         player_data.insert(String::from("rating"), self.rating.to_string());
+        player_data
+    }
+}
+
+impl Player {
+    pub fn from_serdemap(player_data: Map<String, Value>) -> Result<Player, String> {
+        let joueurs_id = player_data
+            .get("joueurs_id")
+            .ok_or("Incomplete player data")?
+            .as_str()
+            .ok_or("Player data format is wrong")?
+            .parse::<i32>()
+            .map_err(|_| String::from("Id is not integer"))?;
+
+        let first_name = player_data
+            .get("first_name")
+            .ok_or("Incomplete player data")?
+            .as_str()
+            .ok_or("Player data format is wrong")?
+            .to_string();
+
+        let last_name = player_data
+            .get("last_name")
+            .ok_or("Incomplete player data")?
+            .as_str()
+            .ok_or("Player data format is wrong")?
+            .to_string();
+
+        let country = player_data
+            .get("country")
+            .ok_or("Incomplete player data")?
+            .as_str()
+            .ok_or("Player data format is wrong")?
+            .to_string();
+
+        let rating = player_data
+            .get("rating")
+            .ok_or("Incomplete player data")?
+            .as_str()
+            .ok_or("Player data format is wrong")?
+            .parse::<i32>()
+            .map_err(|_| String::from("Rating is not integer"))?;
+
+        Ok(Player{ joueurs_id, first_name, last_name, country, rating})
+    }
+
+    pub fn to_serdemap(&self) -> Map<String, Value> {
+        let mut player_data = Map::new();
+        player_data.insert(
+            String::from("joueurs_id"), Value::from(self.joueurs_id.to_string())
+        );
+        player_data.insert(
+            String::from("first_name"), Value::from(self.first_name.clone())
+        );
+        player_data.insert(
+            String::from("last_name"), Value::from(self.last_name.clone())
+        );
+        player_data.insert(
+            String::from("country"), Value::from(self.country.clone())
+        );
+        player_data.insert(
+            String::from("rating"), Value::from(self.rating.to_string())
+        );
         player_data
     }
 }
@@ -127,6 +192,81 @@ mod tests {
             assert_eq!(data.get("last_name").unwrap(), &String::from("last_name_1"));
             assert_eq!(data.get("country").unwrap(),  &String::from("country_SGP"));
             assert_eq!(data.get("rating").unwrap(), &String::from("1250"));
+        }
+    }
+
+    mod test_from_and_to_serdemap {
+        use serde_json::{Map, Value};
+        use crate::tournament_manager::Player;
+
+        #[test]
+        fn test_from_serdemap_complete() {
+            let mut data = Map::new();
+            data.insert(String::from("joueurs_id"), Value::from("132"));
+            data.insert(String::from("first_name"), Value::from("first_name_1"));
+            data.insert(String::from("last_name"), Value::from("last_name_1"));
+            data.insert(String::from("country"), Value::from("SGP"));
+            data.insert(String::from("rating"), Value::from("1815"));
+            let player_result = Player::from_serdemap(data);
+            assert_eq!(player_result.is_ok(), true);
+
+            let player = player_result.unwrap();
+            assert_eq!(player.joueurs_id, 132);
+            assert_eq!(player.first_name, String::from("first_name_1"));
+            assert_eq!(player.last_name, String::from("last_name_1"));
+            assert_eq!(player.country, String::from("SGP"));
+            assert_eq!(player.rating, 1815);
+        }
+
+        #[test]
+        fn test_from_serdemap_incomplete_string_field() {
+            let mut data = Map::new();
+            data.insert(String::from("joueurs_id"), Value::from("132"));
+            data.insert(String::from("first_name"), Value::from("first_name_1"));
+            data.insert(String::from("country"), Value::from("SGP"));
+            data.insert(String::from("rating"), Value::from("1815"));
+            let player_result = Player::from_serdemap(data);
+            assert_eq!(player_result.is_err(), true);
+        }
+
+        #[test]
+        fn test_from_serdemap_incomplete_i32_field() {
+            let mut data = Map::new();
+            data.insert(String::from("first_name"), Value::from("first_name_1"));
+            data.insert(String::from("last_name"), Value::from("last_name_1"));
+            data.insert(String::from("country"), Value::from("SGP"));
+            data.insert(String::from("rating"), Value::from("1815"));
+            let player_result = Player::from_serdemap(data);
+            assert_eq!(player_result.is_err(), true);
+        }
+
+        #[test]
+        fn test_from_serdemap_wrong_field() {
+            let mut data = Map::new();
+            data.insert(String::from("joueurs_id"), Value::from("this_is_not_i32"));
+            data.insert(String::from("first_name"), Value::from("first_name_1"));
+            data.insert(String::from("last_name"), Value::from("last_name_1"));
+            data.insert(String::from("country"), Value::from("SGP"));
+            data.insert(String::from("rating"), Value::from("1815"));
+            let player_result = Player::from_serdemap(data);
+            assert_eq!(player_result.is_err(), true);
+        }
+
+        #[test]
+        fn test_to_serdemap() {
+            let player = Player {
+                joueurs_id: 145,
+                first_name: String::from("first_name_1"),
+                last_name: String::from("last_name_1"),
+                country: String::from("country_SGP"),
+                rating: 1250
+            };
+            let data = player.to_serdemap();
+            assert_eq!(data.get("joueurs_id").unwrap(), &Value::from("145"));
+            assert_eq!(data.get("first_name").unwrap(), &Value::from("first_name_1"));
+            assert_eq!(data.get("last_name").unwrap(), &Value::from("last_name_1"));
+            assert_eq!(data.get("country").unwrap(),  &Value::from("country_SGP"));
+            assert_eq!(data.get("rating").unwrap(), &Value::from("1250"));
         }
     }
 }
