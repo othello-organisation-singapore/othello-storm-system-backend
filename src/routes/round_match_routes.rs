fad2d16dbafe51d7c61781e7b62e3f1a19f67ef5
Maxheart <samuelhenrykurniawan@yahoo.com>
2021-03-30 22:42:51 +0800

    Add round match routes

--- src/routes/round_match_routes.rs
+++ src/routes/round_match_routes.rs
@@ -6,13 +6,81 @@ use crate::response_commands;
 use crate::response_commands::ResponseCommand;
 use crate::utils::get_pooled_connection;
 
-#[get("/<id>/rounds")]
-pub fn get_tournament_rounds(id: i32) -> Json<JsonValue> {
+#[get("/<tournament_id>/rounds")]
+pub fn get_tournament_rounds(tournament_id: i32) -> Json<JsonValue> {
     let connection = get_pooled_connection();
-    response_commands::GetTournamentRoundsCommand { tournament_id: id }.execute(&connection)
+    response_commands::GetTournamentRoundsCommand { tournament_id }.execute(&connection)
 }
 
-#[get("/<tournament_id>/rounds/<round_id>")]
+#[derive(Deserialize)]
+pub struct CreateManualNormalRoundRequest {
+    name: String,
+    match_data: Vec<(i32, i32)>,
+    bye_match_data: Vec<i32>,
+}
+
+#[post("/<tournament_id>/rounds/create_manual_normal", data = "<request>")]
+pub fn create_manual_normal_round(
+    cookies: Cookies,
+    tournament_id: i32,
+    request: Json<CreateManualNormalRoundRequest>,
+) -> Json<JsonValue> {
+    let connection = get_pooled_connection();
+    response_commands::CreateManualNormalRoundCommand {
+        cookies,
+        tournament_id,
+        name: request.name.clone(),
+        match_data: request.match_data.clone(),
+        bye_match_data: request.bye_match_data.clone(),
+    }
+    .execute(&connection)
+}
+
+#[derive(Deserialize)]
+pub struct CreateManualSpecialRoundRequest {
+    name: String,
+    match_data: Vec<(i32, i32)>,
+    bye_match_data: Vec<i32>,
+}
+
+#[post("/<tournament_id>/rounds/create_manual_special", data = "<request>")]
+pub fn create_manual_special_round(
+    cookies: Cookies,
+    tournament_id: i32,
+    request: Json<CreateManualSpecialRoundRequest>,
+) -> Json<JsonValue> {
+    let connection = get_pooled_connection();
+    response_commands::CreateManualSpecialRoundCommand {
+        cookies,
+        tournament_id,
+        name: request.name.clone(),
+        match_data: request.match_data.clone(),
+        bye_match_data: request.bye_match_data.clone(),
+    }
+    .execute(&connection)
+}
+
+#[derive(Deserialize)]
+pub struct CreateAutomaticRoundRequest {
+    name: String,
+}
+
+#[post("/<tournament_id>/rounds/create_manual_special", data = "<request>")]
+pub fn create_automatic_round(
+    cookies: Cookies,
+    tournament_id: i32,
+    request: Json<CreateAutomaticRoundRequest>,
+) -> Json<JsonValue> {
+    let connection = get_pooled_connection();
+    response_commands::CreateAutomaticRoundCommand {
+        cookies,
+        tournament_id,
+        name: request.name.clone(),
+    }
+    .execute(&connection)
+}
+
+#[get("/<_tournament_id>/rounds/<round_id>")]
 pub fn get_round(_tournament_id: i32, round_id: i32) -> Json<JsonValue> {
     let connection = get_pooled_connection();
     response_commands::GetRoundCommand { round_id }.execute(&connection)
@@ -28,28 +96,58 @@ pub fn get_standings(tournament_id: i32, round_id: i32) -> Json<JsonValue> {
     .execute(&connection)
 }
 
-//
-// #[derive(Deserialize)]
-// pub struct TournamentCreationRequest {
-//     name: String,
-//     country: String,
-//     tournament_type: String,
-//     start_date: String,
-//     end_date: String,
-// }
-//
-// #[post("/", data = "<request>")]
-// pub fn create_tournament(
-//     cookies: Cookies,
-//     request: Json<TournamentCreationRequest>,
-// ) -> Json<JsonValue> {
-//     let connection = get_pooled_connection();
-//     response_commands::CreateTournamentCommand {
-//         cookies,
-//         name: request.name.clone(),
-//         country: request.country.clone(),
-//         tournament_type: request.tournament_type.clone(),
-//         start_date: request.start_date.clone(),
-//         end_date: request.end_date.clone(),
-//     }.execute(&connection)
-// }
+#[derive(Deserialize)]
+pub struct UpdateRoundRequest {
+    updated_name: String,
+}
+
+#[patch("/<tournament_id>/rounds/<round_id>", data = "<request>")]
+pub fn update_round(
+    cookies: Cookies,
+    tournament_id: i32,
+    round_id: i32,
+    request: Json<UpdateRoundRequest>,
+) -> Json<JsonValue> {
+    let connection = get_pooled_connection();
+    response_commands::UpdateRoundCommand {
+        cookies,
+        tournament_id,
+        round_id,
+        updated_name: request.updated_name.clone(),
+    }
+    .execute(&connection)
+}
+
+#[get("/<_tournament_id>/rounds/<round_id>/matches")]
+pub fn get_round_matches(_tournament_id: i32, round_id: i32) -> Json<JsonValue> {
+    let connection = get_pooled_connection();
+    response_commands::GetRoundMatchesCommand { round_id }.execute(&connection)
+}
+
+#[derive(Deserialize)]
+pub struct UpdateMatchRequest {
+    black_score: i32,
+    white_score: i32,
+}
+
+#[patch(
+    "/<tournament_id>/rounds/<_round_id>/matches/<match_id>",
+    data = "<request>"
+)]
+pub fn update_match(
+    cookies: Cookies,
+    tournament_id: i32,
+    _round_id: i32,
+    match_id: i32,
+    request: Json<UpdateMatchRequest>,
+) -> Json<JsonValue> {
+    let connection = get_pooled_connection();
+    response_commands::UpdateMatchCommand {
+        cookies,
+        tournament_id,
+        match_id,
+        black_score: request.black_score.clone(),
+        white_score: request.white_score.clone(),
+    }
+    .execute(&connection)
+}
